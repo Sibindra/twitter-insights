2275553a4abb540c867133cc4b4e6ff92f5a9ef0
Aananda-git <bhusal.aananda1@gmail.com>
2023-09-20 14:17:51 +0545

    ..

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,7 @@
 "use client";
-
-
-
 // import { BiGridAlt, BiUser, BiCog } from "react-icons/bi";
 // import { HiMenuAlt2 } from "react-icons/hi";
 // import { useState } from "react";
-
 export default function Dashboard() {
 
 
