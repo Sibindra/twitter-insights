df26f193f73b875c17b1ab2008cf57a6111cba6c
Aananda-git <bhusal.aananda1@gmail.com>
2023-09-20 11:15:08 +0545

    fixed SideBar, layout and routing

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,22 @@
+import SideBar from '@/components/SideBar';
+import React, { ReactNode } from 'react';
+
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <div className="flex flex-row">
+      <div className="flex">
+        <SideBar />
+      </div>
+
+      {/* Main Content */}
+      <div className="flex md:relative w-5/6 bg-secondary h-screen border-2 border-red-500 ml-auto overflow-hidden">
+        {children}
+      </div>
+    </div>
+  );
+}
