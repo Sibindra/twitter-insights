f4a491836219fc843ea9ecad88431d7fb7d0e0c3
Aananda-git <bhusal.aananda1@gmail.com>
2023-09-22 13:50:38 +0545

    configed next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        domains:['avatars.githubusercontent.com', 'pbs.twimg.com', 'abs.twimg.com']
+        domains:['avatars.githubusercontent.com', 'pbs.twimg.com', 'abs.twimg.com','unsplash.com','images.unsplash.com']
     },
 }
 
