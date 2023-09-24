4e80e4f4541c301160bb02a96277e9e7efc81ea9
Sachit Khadka <sachitkhadka73@gmail.com>
2023-09-24 21:34:11 +0545

    hashtag with search field

--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -1,11 +1,47 @@
-import Card from '@/components/cards/hashtag.details'
+// import Card from '@/components/cards/hashtag.details'
+
+// export default function Settings() {
+//   return (
+//     <div>
+//       <Card hashtag={'hindu'} limit={1} />
+//     </div>
+//   )
+// }
+
+'use client';
+import React, { useState } from 'react';
+import Card from '@/components/cards/hashtag.details';
 
 export default function Settings() {
+  const [hashtag, setHashtag] = useState(''); // State for the hashtag
+  const [limit, setLimit] = useState(1); // State for the limit
+
+  const handleSearch = () => {
+    // Perform the search action here using 'hashtag' and 'limit'
+    // You can replace this with your actual search logic
+    console.log(`Searching for hashtag: ${hashtag} with limit: ${limit}`);
+  };
+
   return (
     <div>
-      <Card hashtag={'cricket'} limit={1} />
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Enter hashtag"
+          value={hashtag}
+          onChange={(e) => setHashtag(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Limit"
+          value={limit}
+          onChange={(e) => setLimit(parseInt(e.target.value, 10))}
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
+      <Card hashtag={hashtag} limit={limit} />
+      {/* Other content here */}
     </div>
-  )
+  );
 }
-
 
