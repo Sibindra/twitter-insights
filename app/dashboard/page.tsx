9104357e3b1f4797eb5aa0cda9fdd96fd572e641
Aananda-git <bhusal.aananda1@gmail.com>
2023-09-25 11:28:09 +0545

    skeleton of Dashboard for graphs

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,15 +8,21 @@ export default function Dashboard() {
   return (
     <>
       <div className="flex  flex-col p-10 border border-black bg-white gap-10">
-        <div className="flex gap-3  border border-black">
-          <div className="border border-black">1-graph</div>
+        <div className="flex gap-3  border border-black p-5">
+          <div className="flex flex-1 border border-black">1-graph</div>
 
-          <div className="border border-black">2-graph</div>
+          <div className="flex flex-1 border border-black">2-graph</div>
 
-          <div className="border border-black">3-graph</div>
+          <div className="flex flex-1 border border-black">3-graph</div>
         </div>
-        <div className="flex gap-3 border border-black ">
+
+        <div className="flex gap-3 border border-black bg-slate-100">
           <TweetGraphCard username={username} reply={false} limit={1} />
+        </div>
+
+        <div className="flex gap-3  border border-black p-5">
+          <div className="flex flex-1 border border-black">graph-5</div>
+          <div className="flex flex-1 border border-black">graph-6</div>
         </div>
       </div>
     </>
