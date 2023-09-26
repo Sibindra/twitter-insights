8141b61387bb3978a6b41f6fd6379a1531118cad
Aananda-git <bhusal.aananda1@gmail.com>
2023-09-26 14:23:23 +0545

    changed color

--- components/cards/summary.card.tsx
+++ components/cards/summary.card.tsx
@@ -5,7 +5,7 @@ import { UserDataProps } from "@/lib/user-details";
 export default function BannerCard({ description }: UserDataProps) {
 
   return (
-    <div className="flex flex-col bg-gray-200 p-5">
+    <div className="flex flex-col bg-slate-50 p-5">
       <h1 className="text-lg font-bold mb-3">Summary</h1>
       {description ? (
         <p className="">{description}</p>
