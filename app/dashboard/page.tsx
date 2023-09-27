756e1e7ac287d0818282443c6a25743a48d50462
DeepakRanaMagar <dpk.me.123@gmail.com>
2023-09-27 11:07:05 +0545

    Integrated Sentiment

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,18 +1,22 @@
 "use client";
 
 import RepliesCard from "@/components/cards/replies.card";
-import TrendingHashtagCard from "@/components/cards/trending-hastag.card";
+// import TrendingHashtagCard from "@/components/cards/trending-hastag.card";
 import TweetGraphCard from "@/components/cards/tweet-graph.card";
 import { useAppSelector } from "@/store/store";
-
+import SentimentAnalysisCard from "@/components/cards/sentiment.card";
 export default function Dashboard() {
   const username = useAppSelector((state) => state.username.username);
 
+
+  
   return (
     <>
       {/* <TweetGraphCard username={username} reply={false} limit={1} /> */}
       {/* NEPAL: 2269179 */}
-      <TrendingHashtagCard woeid={2295377}/>
+      <SentimentAnalysisCard input={["I like food", "I don't like food"]} />
+
+      {/* <TrendingHashtagCard woeid={2295377}/> */}
     </>
   );
 }
--- components/cards/trending-hastag.card.tsx
+++ components/cards/trending-hastag.card.tsx
@@ -1,43 +1,43 @@
-'use client';
+// 'use client';
 
-import { TrendingHashtagProps, getTrendingHashtags } from "@/lib/trending-hashtag";
-import { useEffect, useState } from "react";
+// import { TrendingHashtagProps, getTrendingHashtags } from "@/lib/trending-hashtag";
+// import { useEffect, useState } from "react";
 
-export default function TrendingHashtagCard({woeid}: TrendingHashtagProps) {
+// export default function TrendingHashtagCard({woeid}: TrendingHashtagProps) {
 
-    const [trendinghashtagData, setTrendingHastagData] = useState<Array<any> | null>(null);
+//     const [trendinghashtagData, setTrendingHastagData] = useState<Array<any> | null>(null);
 
-    useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const data = await getTrendingHashtags({ woeid});
-            setTrendingHastagData(data);
-          } catch (error) {
-            console.error("Error fetching user data:", error);
-          }
-        };
-
-
-        fetchData();
-    },[woeid])
+//     useEffect(() => {
+//         const fetchData = async () => {
+//           try {
+//             const data = await getTrendingHashtags({ woeid});
+//             setTrendingHastagData(data);
+//           } catch (error) {
+//             console.error("Error fetching user data:", error);
+//           }
+//         };
 
 
-    // console.log(trendinghashtagData)
+//         fetchData();
+//     },[woeid])
 
 
-    const hastagName = trendinghashtagData ? trendinghashtagData[0].trends[0].name : null;
-    const hashtagCount = trendinghashtagData ? trendinghashtagData[0].trends[0].tweet_volume : null;
+//     // console.log(trendinghashtagData)
+
+
+//     const hastagName = trendinghashtagData ? trendinghashtagData[0].trends[0].name : null;
+//     const hashtagCount = trendinghashtagData ? trendinghashtagData[0].trends[0].tweet_volume : null;
     
 
 
-    return(
-      <div>
-        Name: {hastagName}
+//     return(
+//       <div>
+//         Name: {hastagName}
 
-        <br>
-        </br>
+//         <br>
+//         </br>
 
-        Count: {hashtagCount}
-      </div>
-    )
-}
+//         Count: {hashtagCount}
+//       </div>
+//     )
+// }
