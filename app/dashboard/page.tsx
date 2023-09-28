6892f841775f13633463db1231697e61f553c6cc
Sachit Khadka <sachitkhadka73@gmail.com>
2023-09-28 18:13:45 +0545

    graph fix

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,7 @@
 
 import FavCountGraph from "@/components/cards/fav-count.linegraph";
 import TrendingHashtagCard from "@/components/cards/trending-hastag.card";
-import TweetGraphCard from "@/components/cards/tweet-graph.card";
+import TweetGraphCard from "@/components/cards/tweet-linegraph.card";
 import { useAppSelector } from "@/store/store";
 
 export default function Dashboard() {
@@ -10,10 +10,10 @@ export default function Dashboard() {
 
   return (
     <>
-      {/* <TweetGraphCard username={username} reply={false} limit={1} /> */}
+      <TweetGraphCard username={username} reply={false} limit={1} />
       {/* <TrendingHashtagCard woeid={1}/> */}
       {/* <CountGraphCard username={username} reply={false} limit={1}/> */}
-      <FavCountGraph username={username} reply={false} limit={1}/>
+      {/* <FavCountGraph username={username} reply={false} limit={1}/> */}
     </>
   );
 }
