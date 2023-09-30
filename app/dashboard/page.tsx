507c0a2e963372c521260be4dec950cec9cff044
Sachit Khadka <sachitkhadka73@gmail.com>
2023-09-30 13:43:53 +0545

    dash...

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -13,13 +13,30 @@ export default function Dashboard() {
 
   return (
     <>
-      {/* <TweetAreaGraphCard username={username} reply={false} limit={1} /> */}
-      {/* <TrendingHashtagCard woeid={1}/> */}
-      {/* <CountGraphCard username={username} reply={false} limit={1}/> */}
-      {/* <FavCountGraph username={username} reply={false} limit={1}/> */}
-      {/* <FavCountAreaGraph username={username} reply={false} limit={1}/> */}
-      <TweetBarGraphCard username={username} reply={false} limit={1}/>
+      <div className="flex  flex-col p-10 border border-black bg-white gap-10">
+        <div className="flex gap-3  border border-black p-5">
+          <div className="flex flex-1 border border-black">
+            <FavCountAreaGraph username={username} reply={false} limit={1}/>
+          </div>
 
+          <div className="flex flex-1 border border-black">
+            <FavCountGraph username={username} reply={false} limit={1}/>
+          </div>
+
+          <div className="flex flex-1 border border-black">
+            <TweetBarGraphCard username={username} reply={false} limit={1}/>
+          </div>
+        </div>
+
+        <div className="flex gap-3 border border-black bg-slate-100">
+          <TweetGraphCard username={username} reply={false} limit={1} />
+        </div>
+
+        <div className="flex gap-3  border border-black p-5">
+          <div className="flex flex-1 border border-black">graph-5</div>
+          <div className="flex flex-1 border border-black">graph-6</div>
+        </div>
+      </div>
     </>
   );
 }
