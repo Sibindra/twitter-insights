45f92290b97286014122e59c6447046f05656a06
Sibindra <timalsinasibindra@gmail.com>
2023-10-04 21:12:28 +0545

    tweet-card done

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,50 +1,10 @@
 "use client";
 
-import LikeRetweetPieChart from "@/components/cards/graph/likes.retweets.piechart";
-import SentimentAnalysisCard from "@/components/cards/sentiment.card";
-import getTweets, { TweetPromiseProps } from "@/lib/tweets";
 import { useAppSelector } from "@/store/store";
-import { useEffect, useState } from "react";
 
 export default function Dashboard() {
   // controllers
   const username = useAppSelector((state) => state.username.username);
-  const tweet_input = {
-    username: username,
-    reply: false,
-    limit: 1,
-  };
 
-  // FETCH API AND PASS HERE AND REMOVE API CODE FROM ALL THE GRAPHS
-  const [tweetData, setTweetData] = useState<TweetPromiseProps | null>(null);
-  const [error, setError] = useState<string | null>(null);
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const data = await getTweets(tweet_input);
-  //       setTweetData(data);
-  //       setError(null);
-  //     } catch (error) {
-  //       console.error("Error fetching user data:", error);
-  //       setError(
-  //         "An error occurred while fetching data. Please try again later."
-  //       );
-  //     }
-  //   };
-  //   fetchData();
-  // }, [tweet_input]);
-
-  // if (error) {
-  //   return <p>Error: {error}</p>;
-  // }
-
-  // if (!tweetData) {
-  //   return <p>Loading...</p>;
-  // }
-
-  return <>
-  {/* <LikeRetweetPieChart username={username} reply={false} limit={1}/> */}
-  <SentimentAnalysisCard input={['hello' , 'world']}/>
-  </>;
+  return <>Dashboard</>;
 }
--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -7,6 +7,7 @@ import { useQuery } from "@tanstack/react-query";
 import BannerCard from "@/components/cards/banner.card";
 import RecentFollowers from "@/components/cards/followers.card";
 import AdditionalInfoCard from "@/components/cards/additional-details.card";
+import TweetCard from "@/components/cards/tweet.card";
 
 export default function Profile() {
   const username = useAppSelector((state) => state.username.username);
@@ -15,19 +16,17 @@ export default function Profile() {
     queryKey: ["user-details", username],
     queryFn: async () => await getUserDetails({ username }),
   });
-  
+
   const user_id = userData?.user_id as number;
-  
+
   const { data: followerData, status: followerStatus } = useQuery({
     queryKey: ["followers", user_id],
     queryFn: async () => {
       await new Promise((resolve) => setTimeout(resolve, 5000)); // delay for 5 seconds
       return await getUsersFollowers({ user_id: user_id, limit: 1 });
     },
-    staleTime: Infinity, 
+    staleTime: Infinity,
   });
-  
-  
 
   if (userStatus === "loading") {
     return <div>loading... followers...</div>;
@@ -38,21 +37,36 @@ export default function Profile() {
   }
 
   return (
-    <div className="flex flex-col md:flex-row p-5 bg-white gap-10 ">
-      <div className="flex flex-1 gap-2 flex-col ">
-        <div className="">
-          <BannerCard {...userData} />
+    <div className="flex flex-col md:flex-row p-5 bg-secondary gap-3 ">
+      {/* left container */}
+      <div className="flex flex-1 gap-3 flex-col ">
+        {/* BANNER */}
+        <BannerCard {...userData} />
+
+        {/* TWEETS */}
+        <div className="flex flex-col p-5 gap-2 border bg-white ">
+
+         
+         <h3 className=" text-lg font-bold">
+          Most Recent Tweets 
+
+         </h3>
+
+         {/* TODO: 3 for test need to map tweets data here  */}
+        <TweetCard name={'Elon Musk'} username={"elonmusk"} date="27 Jan 2020 5.07am EST" userImg={"https://images.unsplash.com/photo-1682686580036-b5e25932ce9a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3175&q=80"} tweetText={"this is a test tweet text 😱"} comments={10} retweets={50} likes={500} />
+        <TweetCard name={'Elon Musk'} username={"elonmusk"} date="27 Jan 2020 5.07am EST" userImg={"https://images.unsplash.com/photo-1682686580036-b5e25932ce9a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3175&q=80"} tweetText={"this is a test tweet text 😱"} comments={10} retweets={50} likes={500} />
+        <TweetCard name={'Elon Musk'} username={"elonmusk"} date="27 Jan 2020 5.07am EST" userImg={"https://images.unsplash.com/photo-1682686580036-b5e25932ce9a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3175&q=80"} tweetText={"this is a test tweet text 😱"} comments={10} retweets={50} likes={500} />
         </div>
+
       </div>
 
-      <div className="flex  flex-1 gap-1 flex-col ">
-        <div className="">
-          {/* RECENT FOLLOWES WAS HERE */}
-          <RecentFollowers data={followerData?.results} limit={3} />
-        </div>
-        <div className="">
-          <AdditionalInfoCard {...userData} />
-        </div>
+      {/* right container */}
+      <div className="flex  flex-1 gap-3 flex-col ">
+        {/* RECENT FOLLOWERS */}
+        <RecentFollowers data={followerData?.results} limit={5} />
+
+        {/* ADDITIONAL CARDS */}
+        <AdditionalInfoCard {...userData} />
       </div>
     </div>
   );
